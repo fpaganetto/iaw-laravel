47ba42813e795ff64ee35ee6c0b51aa340a2c850
Necdilzor <pachecoagustin@hotmail.com>
2017-05-21 22:31:29 -0300

    Vista autores
    -vista de autores agregado al footer

---
+++ resources/views/autores.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.adderlayout')
+
+@section('content')
+<div class="container main-content">
+<div class="row">
+  <div class="col-xs-12 jumbotron">
+    <h1 class="text-center">Autores</h1>
+    <dl>
+      <dt>Comisión 706</dt>
+      <dd><a href="https://github.com/Nu11Pointer" target="blank">Federico Paganetto - 102225</a></dd>
+      <dd><a href="https://github.com/Necdilzor" target="blank">Agustín Fernando Pacheco Di Santi - 104843</a></dd>
+    </dl>
+  </div>
+</div>
+</div>
+@endsection
